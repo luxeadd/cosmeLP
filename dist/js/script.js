832994aa0724bc46d214a9ce94373f525a7890ae
satoshi kouno <luxeadd@gmail.com>
2022-02-12 12:26:51 +0900

    02121226

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,50 +1,54 @@
 
+//sp時のasideー表示非表示ーーーーーーーーーーーーー
+// let jsAside = document.getElementById('js-aside');
+// let jsMv = document.getElementById('js-mv');
+// let mv_h = jsMv.clientHeight;
+ 
+// window.addEventListener('scroll', () => {
+  // if (window.scrollY >= 80) {
+    // fadeIn(jsAside, 500);
+  // }else {
+    // fadeOut(jsAside, 500);
+  // }
+// });
+
+//アニメーションスクロール判定
+let targets = document.querySelectorAll('.js-scrollAnimation');//アニメーションさせたい要素
+let offset = 100;//アニメーションタイミング
+
+window.addEventListener('scroll', function() {//スクロールしたとき
+
+  var scroll = window.scrollY;//スクロール量を取得
+  var h = window.innerHeight;//画面の高さを取得
+
+  for(let target of targets) {
+    var pos = target.getBoundingClientRect().top + scroll;//アニメーションさせたい要素の位置を取得
+    if (scroll > pos - h + offset) {//スクロール量 > アニメーションさせたい要素の位置
+      target.classList.add('is-animated');
+    }
+  }
+
+});
+
+
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
-  var topBtn = $('.pagetop');
-  topBtn.hide();
+  var aside = $('#js-aside');
+  aside.hide();
 
   // ボタンの表示設定
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 70) {
-      // 指定px以上のスクロールでボタンを表示
-      topBtn.fadeIn();
-    } else {
-      // 画面が指定pxより上ならボタンを非表示
-      topBtn.fadeOut();
-    }
-  });
-
-  // ボタンをクリックしたらスクロールして上に戻る
-  topBtn.click(function () {
-    $('body,html').animate({
-      scrollTop: 0
-    }, 300, 'swing');
-    return false;
-  });
-
-  //ドロワーメニュー
-  $("#MenuButton").click(function () {
-    // $(".l-drawer-menu").toggleClass("is-show");
-    // $(".p-drawer-menu").toggleClass("is-show");
-    $(".js-drawer-open").toggleClass("open");
-    $(".drawer-menu").toggleClass("open");
-    $("html").toggleClass("is-fixed");
-
-  });
-
-
-
-  // スムーススクロール (絶対パスのリンク先が現在のページであった場合でも作動)
-
-  $(document).on('click', 'a[href*="#"]', function () {
-    let time = 400;
-    let header = $('header').innerHeight();
-    let target = $(this.hash);
-    if (!target.length) return;
-    let targetY = target.offset().top - header;
-    $('html,body').animate({ scrollTop: targetY }, time, 'swing');
-    return false;
-  });
-
+  
+  if (window.matchMedia( "(max-width: 768px)" ).matches) {
+    /* ウィンドウサイズが 768px以下の場合のコードをここに */
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 300) {
+        // 指定px以上のスクロールでボタンを表示
+        aside.fadeIn();
+      } else {
+        // 画面が指定pxより上ならボタンを非表示
+        aside.fadeOut();
+      }
+    });
+    } 
+ 
 });
